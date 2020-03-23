51cf08c0c5426f983865646a3b663f77dd96357f
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-23 20:05:46 +0530

    ADDED: code to check for option

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -8,5 +8,29 @@
 StartPosition=0
 endPosition=100
 
+#VARIABLES
+playerPosition=0
+
+
 diceResult=$((RANDOM%6+1))
 echo $diceResult
+
+choice=$((RANDOM%3))
+case $choice in
+			0)
+				#NO PLAY CONDITION
+				playerPosition=$(($playerPosition))
+				echo "NO PLAY CONDITION.."  ;;
+
+			1)
+				#SNAKE
+				playerPosition=$(($diceResult-$playerPosition))
+				echo "GOT SNAKE.."  ;;
+
+			*)
+				#LADDER
+				playerPosition=$(($diceResult+$playerPosition))
+				echo "GOT LADDER.."
+esac
+
+echo "Position of player is:: $playerPosition"
